d92c7e1411849355d775054be01aac1e82ceb93e
shiba_1234 <shibajisahu121@gmail.com>
2024-02-13 00:24:34 +0530

    abstarct and interface

---
+++ src/practice/LearnAbstract.java
@@ -0,0 +1,10 @@
+package practice;
+
+public abstract class LearnAbstract {
+
+    public void nonAbstract() {
+        System.out.println("Non -abstract");
+    }
+
+    public abstract void abstarct();
+}
---
+++ src/practice/demo2.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class demo2 extends LearnAbstract implements LearnInterface{
+    @Override
+    public void abstarct() {
+
+    }
+
+    @Override
+    public void method() {
+
+    }
+}
